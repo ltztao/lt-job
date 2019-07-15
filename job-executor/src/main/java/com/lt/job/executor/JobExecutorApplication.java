81bdbbda4c0114214b8e396f9c387978f512e5bf
msl1221 <shaol.ma@aliyun.com>
2019-07-15 14:12:00 +0800

    first commit lt-job

---
+++ job-executor/src/main/java/com/lt/job/executor/JobExecutorApplication.java
@@ -0,0 +1,12 @@
+package com.lt.job.executor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication public class JobExecutorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobExecutorApplication.class, args);
+    }
+
+}
---
+++ job-executor/src/test/java/com/lt/job/executor/JobExecutorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lt.job.executor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class) @SpringBootTest public class JobExecutorApplicationTests {
+
+    @Test public void contextLoads() {
+    }
+
+}
